628c7775eccafffd8e484725f318d40468a596a3
chichengl <2560720865@qq.com>
2025-03-06 21:36:22 +0800

    2025-03-06 21:36:21

---
+++ lowCode-editor-fe/src/store/editor.ts
@@ -0,0 +1,16 @@
+import { GlobalDataStoreProps } from "./index";
+import { v4 as uuidv4 } from "uuid";
+export interface EditorProps {
+	components: ComponentData[]; // 供中间编辑器渲染的数组
+	currentElement: string; //当前剪辑的哪个元素
+}
+
+interface ComponentData {
+	props: { [key: string]: any };
+	id: string; // uuid v4 生成
+	name: string; //业务组件库名称
+}
+
+export const testComponents: ComponentData[] = [
+	{ id: uuidv4(), name: "text-1", props: { text: "hello1" } },
+];
